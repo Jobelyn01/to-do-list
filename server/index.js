a9091c12d1237bcc7a8637837e22da5d00811e89
Jobelyn01 <jobelynbritos1994@gmail.com>
2026-02-23 16:33:55 +0800

    updated..
    Wrapped listId in parseInt() to ensure it's not NULL in Postgres.

--- server/index.js
+++ server/index.js
@@ -150,7 +150,7 @@ app.get("/get-items/:id", auth, async (req, res) => {
 app.post("/add-item", auth, async (req, res) => {
   const { listId, title } = req.body;
   try {
-    // Changed: Wrapped listId in parseInt() to ensure it's not NULL in Postgres
+   
     await pool.query("INSERT INTO items(list_id, title) VALUES($1, $2)", [parseInt(listId), title]);
     res.json({ success: true });
   } catch (err) { 
